9011430e09b81a79778d9ab1fe34e41a62b2d47b
Dongkyun Kim <dongkyun.kim@voyagerx.com>
2022-01-14 12:20:41 +0900

    fix typo super

--- pytorch_blocks/conv_block.py
+++ pytorch_blocks/conv_block.py
@@ -4,17 +4,17 @@ from pytorch_blocks.normalization import get_normalization
 
 
 class ConvBlock(nn.Sequential):
-    def __init__(self, act, norm, **conv_kargs):
-        super.__init__(
+    def __init__(self, act='none', norm='none', **conv_kargs):
+        super().__init__(
             nn.Conv2d(**conv_kargs),
             get_activation(act),
-            get_normalization(norm),
+            get_normalization(norm, num_features=conv_kargs['out_channels']),
         )
 
 
 class ConvBlocks(nn.Sequential):
-    def __init__(self, num_blocks, dims, act, norm, **conv_kargs):
-        super.__init__()
+    def __init__(self, num_blocks, dims, act='none', norm='none', **conv_kargs):
+        super().__init__()
         assert len(dims) == num_blocks + 1
         for i in range(num_blocks):
             self.add_module(
--- pytorch_blocks/linear_block.py
+++ pytorch_blocks/linear_block.py
@@ -4,21 +4,21 @@ from pytorch_blocks.normalization import get_normalization
 
 
 class LinearBlock(nn.Sequential):
-    def __init__(self, in_dim, out_dim, act, norm):
-        super.__init__(
-            nn.Linear(in_dim, out_dim),
+    def __init__(self, act='none', norm='none', **linear_kwargs):
+        super().__init__(
+            nn.Linear(**linear_kwargs),
             get_activation(act),
-            get_normalization(norm),
+            get_normalization(norm, num_features=linear_kwargs['out_features']),
         )
 
 
 class LinearBlocks(nn.Sequential):
-    def __init__(self, num_blocks, dims, act, norm):
-        super.__init__()
+    def __init__(self, num_blocks, dims, act='none', norm='none', **linear_kwargs):
+        super().__init__()
         assert len(dims) == num_blocks + 1
         for i in range(num_blocks):
             self.add_module(
                 f'linear_block_{i}',
-                LinearBlock(dims[i], dims[i + 1], act, norm)
+                LinearBlock(act, norm, in_features=dims[i], out_features=dims[i + 1], **linear_kwargs)
             )
 
