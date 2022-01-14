4d661d51a4ab5a627d476933c0188551aad734e0
Dongkyun Kim <dongkyun.kim@voyagerx.com>
2022-01-14 11:41:14 +0900

    add __init__.py

--- blocks/__init__.py
+++ blocks/__init__.py
@@ -2,3 +2,4 @@ from blocks.activation import get_activation
 from blocks.normalization import get_normalization
 from blocks.conv_block import ConvBlock, ConvBlocks
 from blocks.linear_block import LinearBlock, LinearBlocks
+
