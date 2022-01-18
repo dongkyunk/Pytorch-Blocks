e297e983209e691a74fe55534d709c3c63438852
Dongkyun Kim <dongkyun.kim@voyagerx.com>
2022-01-18 11:05:19 +0900

    fix convblock norm

--- pytorch_blocks/conv_block.py
+++ pytorch_blocks/conv_block.py
@@ -1,9 +1,9 @@
 import torch.nn as nn
-from einops import rearrange
+from einops.layers.torch import Rearrange
 from pytorch_blocks.activation import get_activation
 from pytorch_blocks.normalization import get_normalization
 
-class ConvBlock(nn.Module):
+class ConvBlock(nn.Sequential):
     """Convolutional block with activation and normalization.
 
     Args:
@@ -13,30 +13,24 @@ class ConvBlock(nn.Module):
     """    
     def __init__(self, act='none', norm='none', **conv_kargs):        
         assert norm in ['bn2d', 'ln', 'in2d', 'none'], "Unsupported image normalization function."
-        super().__init__()
         if norm in ['bn2d', 'in2d']:
             self.norm = get_normalization(norm, num_features=conv_kargs['out_channels'])
             conv_kargs['bias'] = False
         elif norm == 'ln':
-            # Layer normalization is done on the channel dimension only
-            self.norm = get_normalization(norm, normalized_shape=conv_kargs['out_channels'])
-            self.reshape = True
+            self.norm = nn.Sequential(
+                Rearrange('b c h w -> b h w c'),
+                get_normalization(norm, normalized_shape=conv_kargs['out_channels']), # channel dimension only
+                Rearrange('b h w c -> b c h w'),
+            )
         elif norm == 'none':
             self.norm = get_normalization(norm)
-        self.conv = nn.Conv2d(**conv_kargs)
-        self.act = get_activation(act)
 
+        super().__init__(
+            nn.Conv2d(**conv_kargs),
+            self.norm,
+            get_activation(act),
+        )
 
-    def forward(self, x):
-        x = self.conv(x)
-        if self.reshape:
-            x = rearrange(x, 'b c h w -> b h w c')
-            x = self.norm(x)
-            x = rearrange(x, 'b h w c-> b c h w')
-        else:
-            x = self.norm(x)
-        x = self.act(x)
-        return x
 
 
 class ConvBlocks(nn.Sequential):
